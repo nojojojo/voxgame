726480a11ed61dd3b5c13f68df800d8b17c86c68
nojojojo <noah.johanson@gmail.com>
2017-06-19 23:55:57 -0700

    First commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed = 6.0F;
+	public float rotateSpeed = 6.0F;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CharacterController controller = GetComponent<CharacterController>();
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection (Vector3.forward);
+		float curSpeed = speed * Input.GetAxis ("Vertical");
+		controller.SimpleMove (forward * curSpeed);
+		
+	}
+}		
+
+
